b44420bc52cff2ffa52e31c77a9f7578f9e81c17
Oliver Buckley <30906123+ApalloAltoBluck@users.noreply.github.com>
2018-01-26 15:18:30 -0500

    Reformatted Grid System,
    Big step yur

--- index.php
+++ index.php
@@ -73,15 +73,18 @@
       <div class ="col s9">
       <script type="text/javascript" src="js/materialize.min.js"></script>
       </div>
-      <h1>Lorem Ipsun</h1>
       <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
       <script>
             $(".button-collapse").sideNav();
       </script>
       </div>
-      <div class = "Gallery">
+
+
+    
+      <div class = "Container">
           <div class = "Gallery-item">
+          <?php for($x=1; $x < 13; $x++): ?>
                   <div class="card-reveal">
                       <div class="card">
                           <div class="card-image waves-effect waves-block waves-light">
@@ -95,10 +98,14 @@
                             <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
                             <p>Here is some more information about this product that is only revealed once clicked on.</p>
                           </div>
-                        </div>
+                        </div> 
                   </div>
+
+                  
+                  <?php endfor; ?>
                 </div>
           </div>
        </div>
+       
     </body>
   </html>
