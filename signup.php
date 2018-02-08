111e6f82127e540cc49efd2a2bf7700315431477
Oliver Buckley <30906123+ApalloAltoBluck@users.noreply.github.com>
2018-02-07 20:28:20 -0500

    Sign Up work

--- signup.php
+++ signup.php
@@ -1,27 +1,39 @@
 <!DOCTYPE html>
 <html>
   <head class="SignupClass">
+  
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css--> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-    <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Sign Up</title>
   </head>
-  <style>
-      background-image:url("https://webgradients.com/public/webgradients_png/035%20Itmeo%20Branding.png");
+  <body>
+ 
 
-  </style>
+  <div class="row">
+      <div class="col s6">
+      <style>
+      
 
-  <body >
-        <h1 class="center align" >Create a log</h1>
+      </style>
+      
+      </div>
+      
+      <div class="col s6"><h1>Create a log</h1></div>
+  </div>
+
+
+
 
    <!-- Scripts !-->
    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-    
+   <link rel="stylesheet" href="style.css">
+
   </body>
 </html>
