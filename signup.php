8b539d6143b9cf82537551824d8d1135f268b0ba
ApalloAltoBluck <30906123+ApalloAltoBluck@users.noreply.github.com>
2018-02-08 22:02:14 -0500

    Beta page

--- signup.php
+++ signup.php
@@ -3,6 +3,7 @@
   <head class="SignupClass">
   
     <!--Import Google Icon Font-->
+    
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css--> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
@@ -16,24 +17,31 @@
  
 
   <div class="row">
+
+  <form class="col s12">
+
+
       <div class="col s6">
+     <img width="800px"; class="zindex" src="Createlog.png">
+    <img class="zindex2" width=1000px; height=1360px; src="signupgradient.png" >
 
-    <img width=1000px; height=960px; src="signupgradient.png" >
-
-  
-      
       </div>
       
-      <div class="col s6"><h1>Create a log</h1></div>
-  </div>
+        <h1 class="logleft">Sign Up</h1>
+    
+        <div class="input-field col s6 alignform">
+          <input width=400px; height=40px; placeholder="First Name" id="first_name" type="text" class="validate">
+          <label for="first_name">First Name</label>
+    </div>
 
 
 
 
    <!-- Scripts !-->
+   <link rel="stylesheet" href="style.css">
    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-   <link rel="stylesheet" href="style.css">
+
 
   </body>
 </html>
