85df7fbf077b61276d3366a59194b954c3ad478b
Oliver Buckley <30906123+ApalloAltoBluck@users.noreply.github.com>
2018-02-15 22:19:44 -0500

    Working on sign up

--- index.php
+++ index.php
@@ -48,61 +48,31 @@
                     <div class="nav-wrapper">
                         <form>
                             <div class="input-field bluexx`">
-                                <input id="search" type="search" required>
-                                <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+                                
+                                <form action = "includes/login.inc.php" method = "POST">
+                                <input type = "text" name= "uid" placeholder="Username/e-mail">
+                                <input type = "password" name= "pwd" placeholder="password">
+                                <button class="btn btn-primary #80cbc4 teal lighten-3" type = "submit" name  = "submit">Login</button>
+                            </form>
+                            <button class = "btn btn-primary #80cbc4 teal lighten-3" href="signup.php">Sign up</button>
                                 <i class="material-icons">close</i>
                             </div>
                         </form>
                     </div>
                 </nav>
-                <li><a href="#!">First Sidebar Link</a></li>
-                <li><a href="#!">Second Sidebar Link</a></li>
+               
             </ul>
 
+<?php 
 
-
-
+if (isset($_SESSION['u_id'])){
+    include_once 'loggedinheader.php';
+} else {
+ include_once 'notloggedinheader.php';
+}
+?>
 
             
-            <div class="button-group filter-button-group">
-            <a href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="large material-icons">menu</i></a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <div class="filter">
-            
-            <button data-name='*' class="btn btn-primary #80cbc4 teal lighten-3 " >All</button>
-            <button data-name='.Nepenthes' class="btn btn-primary #80cbc4 teal lighten-3">Nepenthes</button>
-            <button data-name='.Heliamphora' class="btn btn-primary #80cbc4 teal lighten-3">Heliamphora</button>
-            <button data-name='*' class="btn btn-primary #80cbc4 teal lighten-3">Drosera</button>
-            <button data-name='*' class="btn btn-primary #80cbc4 teal lighten-3">Dionaea</button>
-            <button data-name='*' class="btn btn-primary #80cbc4 teal lighten-3">Cephalotus</button>
-            <button data-name='*' class="btn btn-primary #80cbc4 teal lighten-3">Sarracenia</button>
-            <button data-name='*' class="btn btn-primary #80cbc4 teal lighten-3">Pinguicula</button>
-            <button data-name='*' class="btn btn-primary #80cbc4 teal lighten-3">Utricularia</button>
-                <div class="grid">
-
-                
-                <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
-                <ul id="dropdown1" class="dropdown-content">
-                    <li><a href="#!">Drosphyllum</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#!">Aldrovanda</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#!">Darlingtonia</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#!">Roridula</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#!">Genlisea</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#!">Byblis</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#!">Other</a></li>
-                </ul>
-            </ul>
-            
-        </div>
-        </div>
-        </div>
-    </nav>
 
     <div class="col s9">
         <script type="text/javascript" src="js/materialize.min.js"></script>
---
+++ loggedinheader.php
@@ -0,0 +1,41 @@
+
+            <div class="button-group filter-button-group">
+            <a href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="large material-icons">menu</i></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <div class="filter">
+            
+            <button data-name='*' class="btn btn-primary #80cbc4 teal lighten-3 " >All</button>
+            <button data-name='.Nepenthes' class="btn btn-primary #80cbc4 teal lighten-3">Nepenthes</button>
+            <button data-name='.Heliamphora' class="btn btn-primary #80cbc4 teal lighten-3">Heliamphora</button>
+            <button data-name='*' class="btn btn-primary #80cbc4 teal lighten-3">Drosera</button>
+            <button data-name='*' class="btn btn-primary #80cbc4 teal lighten-3">Dionaea</button>
+            <button data-name='*' class="btn btn-primary #80cbc4 teal lighten-3">Cephalotus</button>
+            <button data-name='*' class="btn btn-primary #80cbc4 teal lighten-3">Sarracenia</button>
+            <button data-name='*' class="btn btn-primary #80cbc4 teal lighten-3">Pinguicula</button>
+            <button data-name='*' class="btn btn-primary #80cbc4 teal lighten-3">Utricularia</button>
+                <div class="grid">
+
+                
+                <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
+                <ul id="dropdown1" class="dropdown-content">
+                
+                    <li><a href="#!">Drosphyllum</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#!">Aldrovanda</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#!">Darlingtonia</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#!">Roridula</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#!">Genlisea</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#!">Byblis</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#!">Other</a></li>
+                </ul>
+            </ul>
+            
+        </div>
+        </div>
+        </div>
+    </nav>
